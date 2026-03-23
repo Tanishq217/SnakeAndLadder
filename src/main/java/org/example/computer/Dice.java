96d49940498a43d6045b1a7f1bda338ff9cb6f50
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2026-03-24 04:12:30 +0530

    Pushed Main Class with Made Dice Class that  Generate random numbers for movement. Single Responsibility Principle: Dice only handles random number generation also Easy to change dice type (different number of sides)

---
+++ src/main/java/org/example/computer/Dice.java
@@ -0,0 +1,21 @@
+package org.example.computer;
+
+import java.util.Random;
+
+public class Dice {
+    private final int sides;
+    private final Random random;
+
+    public Dice(int sides) {
+        this.sides = sides;
+        this.random = new Random();
+    }
+
+    public int roll() {
+        return random.nextInt(sides) + 1;
+    }
+
+    public int getSides() {
+        return sides;
+    }
+}
