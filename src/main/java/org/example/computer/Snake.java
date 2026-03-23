e893df3ac278a89b9f179338fce9836f3245986f
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2026-03-24 04:09:36 +0530

    Made Snake and Ladder classes ! Why validation in constructor?
    A snake must go DOWN (start > end)
    
    A ladder must go UP (start < end)
    
    This is defensive programming - catch errors early when object is created
    
    final - Snakes and ladders shouldn't change during game

---
+++ src/main/java/org/example/computer/Snake.java
@@ -0,0 +1,27 @@
+package org.example.computer;
+
+public class Snake {
+    private final int start;
+    private final int end;
+
+    public Snake(int start, int end) {
+        if (start <= end) {
+            throw new IllegalArgumentException("Snake start must be greater than end");
+        }
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    @Override
+    public String toString() {
+        return "Snake: " + start + " -> " + end;
+    }
+}
